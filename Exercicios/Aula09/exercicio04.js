d47290fa29c70f134c0c3096db69f818d8b57304
Carlos Eduardo Gonzaga <91717487+gonzaga95@users.noreply.github.com>
2022-06-05 20:40:33 -0400

    melhorando exercicio4 aula 09

--- Exercicios/Aula09/exercicio04.js
+++ Exercicios/Aula09/exercicio04.js
@@ -11,23 +11,36 @@ let eleitores = +prompt('Digite o número total de eleitores: ');
 console.clear();
 console.log(`O total de eleitores foi ${eleitores}.`);
 
-while (eleitores > 0){
-    for (let i = 0; i < eleitores; i++){
-        let escolhido = Math.ceil(Math.random() *3);
-        switch (escolhido) {
-            case 1:
-                candidato1++
-                break;
-            case 2:
-                candidato2++
-                break;
-            case 3: 
-                candidato3++;
-                break;
-        }
-        eleitores--
+for (let i = eleitores; i > 0; i--){
+    // let escolhido = Math.ceil(Math.random() *3); // Votos aleatórios para testar grandes números.
+    console.log(`Escolha o candidato1, candidato2 ou candidato3.`);
+    let escolhido = prompt('');
+    while (escolhido != 'candidato1' && escolhido != 'candidato2' && escolhido != 'candidato3') {
+        console.log('Opção inválida.')
+        console.log(`Escolha o candidato1, candidato2 ou candidato3.`);
+        escolhido = prompt('');
     }
-}
-console.log(`O candidato 1 teve ${candidato1} votos.
+    switch (escolhido) {
+        // case 1: // Para votos aleatórios conforme linha 15
+        case 'candidato1':
+            candidato1++
+            break;
+        // case 2: // Para votos aleatórios conforme linha 15
+        case 'candidato2':
+            candidato2++
+            break;
+        // case 3: // Para votos aleatórios conforme linha 15
+        case 'candidato3':
+            candidato3++;
+            break;
+    }
+    console.clear();
+    console.log('Voto computado!');
+    console.log();
+    console.log(`Restam ${i-1} eleitores.`)
+    if (i == 1) {
+        console.log(`O candidato 1 teve ${candidato1} votos.
 O candidato 2 teve ${candidato2} votos.
 O candidato 3 teve ${candidato3} votos.`);
+    }
+}
