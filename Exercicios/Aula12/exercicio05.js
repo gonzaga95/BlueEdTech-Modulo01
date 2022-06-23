caa1b2bc3b50b3057a8dafdef9a44b189cc8f5c0
Carlos Eduardo Gonzaga <91717487+gonzaga95@users.noreply.github.com>
2022-06-22 21:31:13 -0400

    concluindo exercicios aula 14

--- Exercicios/Aula12/exercicio05.js
+++ Exercicios/Aula12/exercicio05.js
@@ -1,2 +1,23 @@
 const prompt = require("prompt-sync")();
 console.clear();
+
+// Crie uma função para inverter um array passado pelo usuário.
+
+const array = [];
+
+function arrayInverter(a){
+    let invertida = a.reverse();
+    return invertida;    
+}
+
+console.log('Você deve inserir uma lista e a mesma será aprensentada em ordem inversa');
+const tamanhoArray = +prompt('Quantos elementos terá a lista? ');
+
+for (i = 0; i < tamanhoArray; i++){
+    let elemento = prompt(`Qual é o ${i+1}º elemento? `);
+    array.push(elemento);
+}
+
+console.log();
+console.log('A array invertida é:')
+console.log(arrayInverter(array));
