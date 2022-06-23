32dda56ed37538b32a5161fc2400f1dc7d97352d
Carlos Eduardo Gonzaga <91717487+gonzaga95@users.noreply.github.com>
2022-06-22 22:32:28 -0400

    projeto extra aula 14 start

---
+++ Exercicios/Aula12/projeto-extra-aula12.js
@@ -0,0 +1,96 @@
+console.clear();
+const prompt = require("prompt-sync")();
+
+// A ideia deste projeto é criar um programa que simule uma votação com todo o conteúdo visto no módulo
+// até este momento.
+// O programa deve:
+// Receber votos até que o usuário diga que não tem mais ninguém para votar;
+// Ter uma função chamada autorizaVoto (anoNascimento) retornando: “Negado”, “Facultativo” ou “Obrigatório”;
+// Ter uma função chamada votação (autorização, voto) que valida e contabiliza o voto (número entre 1 e 4 ou anulado)
+// ou retorna a mensagem: “Você não pode votar”, caso o voto não possa ser contabilizado;
+// Contabilizar os votos conforme os significados:
+// 1 = Candidato 1
+// 2 = Candidato 2
+// 3 = Candidato 3
+// 4 = Voto em branco
+// Outro = voto nulo
+// Ter uma função chamada exibirResultados() que deve mostrar:
+// O total de votos para cada candidato
+// O total de votos em branco
+// O total de votos nulos
+// Qual candidato venceu a votação
+
+let votacaoAberta = true;
+
+let candidatos = [
+    ["Candidato 1", "número =>", 1],
+    ["Candidato 2", "número =>", 2],
+    ["Candidato 3", "número =>", 3],
+    ["Branco", "número =>", 4],
+];
+
+let candidato1 = 0;
+let candidato2 = 0;
+let candidato3 = 0;
+let votoBranco = 0;
+let votoAnulado = 0;
+
+function autorizaVoto(ano) {
+    let idade = 2022 - ano;
+    if (idade < 16) {
+        permissao = false;
+        return "Voto negado";
+    } else {
+        permissao = true;
+        if (idade < 18) {
+            return "Voto facultativo";
+        }
+        return "Voto obrigatório";
+    }
+}
+
+function votacao(autorizacao, voto) {
+    if (autorizacao) {
+        switch (voto) {
+            case 1:
+                candidato1++;
+                break;
+            case 2:
+                candidato2++;
+                break;
+            case 3:
+                candidato3++;
+                break;
+            case 4:
+                votoBranco++;
+                break;
+            default:
+                votoAnulado++;
+                break;
+        }
+        console.log("Registrado");
+    } else {
+        console.log("Você não pode votar");
+    }
+}
+
+while (votacaoAberta) {
+    console.log(
+        "Você tem a opção de tais canditatos, seguidos de seus respectivos números na urna"
+    );
+    console.table(candidatos);
+
+    while (true) {
+        let continuar = prompt(
+            "Deseja inserir mais um voto? [S/N] "
+        ).toUpperCase();
+        if (continuar === "S") {
+            break;
+        } else if (continuar === "N") {
+            votacaoAberta = false;
+            break;
+        }
+        console.log("Responda S para SIM");
+        console.log("Responda N para NÃO");
+    }
+}
